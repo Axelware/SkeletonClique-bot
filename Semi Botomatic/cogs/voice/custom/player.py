f0bda616dd704ee4c1ab686c97029c02fb3214f2
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-01-16 21:28:10 +0000

    increase player timeout

--- Semi Botomatic/cogs/voice/custom/player.py
+++ Semi Botomatic/cogs/voice/custom/player.py
@@ -218,7 +218,7 @@ class Player(slate.Player):
 
             if self.queue.is_empty:
 
-                timeout = 10
+                timeout = 120
 
                 try:
                     with async_timeout.timeout(timeout=timeout):
