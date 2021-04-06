a4348c6c227f9fff9a36544de3f9f16890436ae7
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-04-06 21:56:19 +0100

    fix error with dpy 2.0

--- Semi Botomatic/bot.py
+++ Semi Botomatic/bot.py
@@ -26,7 +26,7 @@ class SemiBotomatic(commands.AutoShardedBot):
     def __init__(self) -> None:
         super().__init__(
                 command_prefix=commands.when_mentioned_or(config.PREFIX), help_command=help.HelpCommand(), owner_ids=config.OWNER_IDS, intents=discord.Intents.all(),
-                activity=discord.Activity(type=discord.ActivityType.watching, name='all of you'), max_messages=10000,
+                activity=discord.Activity(type=3, name='all of you'), max_messages=10000,
                 allowed_mentions=discord.AllowedMentions(everyone=False, users=True, roles=True, replied_user=True)
         )
 
